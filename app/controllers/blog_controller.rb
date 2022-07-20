849a034f3f0eb8df55abd6832758d75477d25c93
pkamano <p.amano1225@protonmail.com>
2022-07-20 16:46:31 -0700

    done with the blog posts

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -6,9 +6,21 @@ class BlogController < ApplicationController
         @blog = Blog.find(params[:id])
     end
     def new
+        @blog = Blog.new
     end
     def create
-        @blog = Blog.create(title: params[:title], content: params[:content]
-        )
+        @blog = Blog.create(entry_params)
+        puts '#{entry_params}'
+        if @blog.valid?
+            redirect_to blogs_path
+        else
+            redirect_to entry_path
+        end
+    end
+
+    private
+    def entry_params
+        params.require(:blog).permit(:title, :content) 
+
     end
 end
