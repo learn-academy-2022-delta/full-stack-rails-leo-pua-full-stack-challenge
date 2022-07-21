095cf0d3a258ae30a99089c7eb40251cb18521e9
pkamano <p.amano1225@protonmail.com>
2022-07-21 13:05:06 -0700

    blog post challenge

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -17,7 +17,26 @@ class BlogController < ApplicationController
             redirect_to entry_path
         end
     end
-
+    
+    def edit
+        @blog = Blog.find(params[:id])
+        # if @blog.edit
+        #     redirect_to edit_path(@blog)
+        # end
+    end
+    def update
+        @blog = Blog.find(params[:id])
+        @blog.update(entry_params)
+        if @blog.valid?
+            redirect_to blog_path(@blog)
+        end
+    end
+    def destroy
+        @blog = Blog.find(params[:id])
+        if @blog.destroy 
+            redirect_to blogs_path
+        end
+    end
     private
     def entry_params
         params.require(:blog).permit(:title, :content) 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
 get 'blogs' => 'blog#index', as: 'blogs'
-post 'blogs' => 'blog#create'
+
 get 'blogs/new' => 'blog#new', as: 'entry'
+
+post 'blogs' => 'blog#create'
+
 get 'blogs/:id' => 'blog#show', as: 'blog'
 
+get 'blogs/:id/edit' => 'blog#edit', as: 'edit'
+
+patch 'blogs/:id' => 'blog#update'
+
+delete 'blogs/:id' => 'blog#destroy', as: 'delete_blog'
 end
+
+
+
